5b916d31195e8ebd931fa06ec1ef66158de9b69a
sungyeeun <yeeun2486@gmail.com>
2024-01-15 21:47:10 +0900

    LoginWidget/LoginGameModeBase 완성 - 성공확인완료

--- Source/MultiTeamProject/Public/NetworkGameInstance.h
+++ Source/MultiTeamProject/Public/NetworkGameInstance.h
@@ -10,6 +10,12 @@
 /**
  * 
  */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FSearchSessionDele, FString, roomName, FString, hostName, int32, currentPlayers, int32, maxPlayers, int32, ping, int32, sessionIdx);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FServerResponseDele);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFindButtonStatus, bool, bIsEnabled);
+
 UCLASS()
 class MULTITEAMPROJECT_API UNetworkGameInstance : public UGameInstance
 {
@@ -18,19 +24,30 @@ class MULTITEAMPROJECT_API UNetworkGameInstance : public UGameInstance
 public:
 	virtual void Init() override;
 
-	//세션 인터페이스 변수
+	// 세션 인터페이스 변수
+	//TSharedPtr<IOnlineSession, ESPMode::ThreadSafe> sessionInterface;
 	IOnlineSessionPtr sessionInterface;
 
-	//몇명까지 서버에 접속할 수 있도록 할 것인가 -> playerRange
-	void CreateSession(FString sessionName, FString hostName, int32 playerCount);
+	UPROPERTY(EditAnywhere, BlueprintAssignable, Category = "MySettings")
+	FSearchSessionDele onCreateSlot;
+
+	UPROPERTY(EditAnywhere, BlueprintAssignable, Category = "MySettings")
+	FServerResponseDele onNewSearchComplete;
+
+	UPROPERTY(EditAnywhere, BlueprintAssignable, Category = "MySettings")
+	FFindButtonStatus onFindButtonToggle;
+
+	void CreateSession(FString roomName, FString hostName, int32 playerCount);
 	void FindSession();
+	void JoinSession(int32 roomNumber);
 
 private:
-	FName mySessionName = FName("sessac session");
+	FName mySessionName = FName("Sessac Session");
 	TSharedPtr<FOnlineSessionSearch> sessionSearch;
 
-	void OncreatedSession(FName roomName, bool bWasSuccessful);
+	void OnCreatedSession(FName sessionName, bool bWasSuccessful);
 	void OnFoundSessions(bool bWasSuccessful);
+	void OnJoinedSession(FName sessionName, EOnJoinSessionCompleteResult::Type result);
 };
 
 
