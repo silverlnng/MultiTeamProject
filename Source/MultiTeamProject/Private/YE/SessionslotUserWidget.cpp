1f7fec73ee451691c1abd36093d804ced66397ca
sungyeeun <yeeun2486@gmail.com>
2024-01-17 22:39:23 +0900

    sessionslotwidget 생성

---
+++ Source/MultiTeamProject/Private/YE/SessionslotUserWidget.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "YE/SessionslotUserWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "NetworkGameInstance.h"
+
+void USessionslotUserWidget::NativeConstruct()
+{
+}
+
+void USessionslotUserWidget::SetSessionInfo(const FString roomName, const FString hostName, const int32 curPlayer, const int32 maxPlayer, const int32 pingSpeed, const int32 idx)
+{
+}
